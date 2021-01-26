6ce3e0efbba7dd000443ab3fcad890f1b3793584
Van Darrell Ponce <riversideriver@yahoo.com>
2021-01-26 17:46:53 +0800

    initial setup for react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,23 @@ import Navbar from './components/Navbar'
 import People from './components/People'
 import Planets from './components/Planets'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
+
 const App = (props) => {
   const [page, setPage] = useState('planets')
   useEffect(() => {}, [])
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === 'planets' ? <Planets /> : <People />}
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   )
 }
 
--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+
+const fetchPlanets = async () => {
+  const res = await fetch('http://swapi.dev/api/planets/')
+  return res.json()
+}
 
 const Planets = () => {
-	return <div> Planets</div>
+  const { data, status } = useQuery('planets', fetchPlanets)
+  console.log({ data, status })
+  return <div> Planets</div>
 }
 
 export default Planets
